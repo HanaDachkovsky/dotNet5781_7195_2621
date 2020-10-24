a4213d67dfc18092f2c73ecb508278e25b133910
Hana Dachkovsky <dachkovs@g.jct.ac.il>
2020-10-24 22:15:31 +0300

    intial code, part 1.

---
+++ dotNet5781_00_7195_2621/Program7195.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_7195_2621
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome7195();
+            Console.ReadKey();
+        }
+        static partial void Welcome2621();
+        private static void Welcome7195()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
