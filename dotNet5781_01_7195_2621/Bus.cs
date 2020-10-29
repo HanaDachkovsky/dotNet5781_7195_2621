9bc04d6cd888ca4e6781490daf6f2cf222d44f26
Hana Dachkovsky <dachkovs@g.jct.ac.il>
2020-10-29 16:45:34 +0200

    insert class bus, make list and menu in the main

---
+++ dotNet5781_01_7195_2621/Bus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_7195_2621
+{
+    class Bus
+    {
+        private string vehicleNum;
+        private DateTime startDate;
+        private DateTime lastCare;
+        private double lastKm;
+        private double kilometrage;
+        private double availableKm;
+        public Bus()
+        {
+
+        }
+       
+    }
+}
--- dotNet5781_01_7195_2621/Program.cs
+++ dotNet5781_01_7195_2621/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Services;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,30 @@ namespace dotNet5781_01_7195_2621
     {
         static void Main(string[] args)
         {
+            List<Bus>ourBuses;
+            int choice;
+            Console.WriteLine("Enter your choice:");
+            Console.WriteLine("1: add a bus");
+            Console.WriteLine("2: choose a bus to drive");
+            Console.WriteLine("3: to care or to refueling");
+            Console.WriteLine("4: print the kilometers from the last care, from all the buses in the company");
+            Console.WriteLine("0: to exit");
+            choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    break;
+                case 2:
+                    
+                default:
+                    break;
+            }
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
