731088bb728f908ff68ca34bb77d0abd965b2b87
Hana Dachkovsky <dachkovs@g.jct.ac.il>
2020-10-29 15:21:11 +0200

    new project 01

---
+++ dotNet5781_01_7195_2621/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_7195_2621
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
