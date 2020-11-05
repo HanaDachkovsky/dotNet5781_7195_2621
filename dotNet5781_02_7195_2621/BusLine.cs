a6cdfa487fce9bb15eb023e6b1ba448737f0ae8c
Hana Dachkovsky <dachkovs@g.jct.ac.il>
2020-11-05 18:19:22 +0200

    add buslinestation and busline classes

---
+++ dotNet5781_02_7195_2621/BusLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_7195_2621
+{
+    enum AREA { General,North,South,Center,Jerusalem};
+    class BusLine
+    {
+        private List<BusLineStation> stations;
+        private int busLine;
+        private BusStation firstStstion;
+        private BusStation lastStation;
+        private AREA area;
+
+        
+    }
+}
--- dotNet5781_02_7195_2621/BusLineStation.cs
+++ dotNet5781_02_7195_2621/BusLineStation.cs
@@ -8,6 +8,8 @@ namespace dotNet5781_02_7195_2621
 {
     class BusLineStation:BusStation
     {
-
+        private double distanceFromPrevStat;//distance from previous station
+        private TimeSpan timeFromPrevStat;//time from the previous station
+        //בנאים
     }
 }
