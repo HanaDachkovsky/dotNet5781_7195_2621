6eb6a975cb8da61706c456208d4d39897afa964e
Hana Dachkovsky <dachkovs@g.jct.ac.il>
2020-11-05 21:30:05 +0200

    insert checkstation insertstation and subpath functions

--- dotNet5781_02_7195_2621/BusLine.cs
+++ dotNet5781_02_7195_2621/BusLine.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,73 @@ namespace dotNet5781_02_7195_2621
         private BusStation lastStation;
         private AREA area;
 
-        
+        internal BusStation FirstStstion { get => firstStstion; set => firstStstion = value; }
+        internal BusStation LastStation { get => lastStation; set => lastStation = value; }
+        internal AREA Area { get => area; set => area = value; }
+
+        //בנאי
+        public override string ToString()
+        {
+            string data;
+            data = busLine + " " + area+" ";
+            foreach (var item in stations)
+            {
+                data += item.BusStationKey;
+                data += " ";
+            }
+            return data;
+        }
+        public bool CheckStation(BusLineStation station)
+        {
+            for(int i=0;i<stations.Count;i++)
+            {
+                if(stations[i]==station)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void InsertStation(BusLineStation station)
+        {
+            for (int i = 0; i < stations.Count; i++)
+            {
+                if (stations[i].BusStationKey == station.BusStationKey)
+                {
+                    //לזרוק חריגה
+                }
+            }
+
+
+        }
+        public BusLine SubPath(BusLineStation firstStation, BusLineStation secondStation)
+        {
+            BusLine subBus;
+            if(CheckStation(firstStation)&&CheckStation(secondStation))
+            {
+                int i;
+                for ( i = 0; i < stations.Count; i++)
+                {
+                    if (stations[i] == firstStation)
+                    {
+                        break;
+                    }
+                }
+                while( stations[i]!=secondStation )
+                {
+                    subBus.InsertStation(stations[i]);
+                }
+                subBus.InsertStation(secondStation);
+                subBus.FirstStstion=firstStation;
+                subBus.LastStation = secondStation;
+                subBus.area = area;
+            }
+            else
+            {
+                //לזרוק חריגה
+            }
+
+        }
+
     }
 }
--- dotNet5781_02_7195_2621/BusStation.cs
+++ dotNet5781_02_7195_2621/BusStation.cs
@@ -13,6 +13,9 @@ namespace dotNet5781_02_7195_2621
         private double longitude;//קו אורך  
         private string adress;
         static private Random rand = new Random(DateTime.Now.Millisecond);
+
+        public int BusStationKey { get => busStationKey; set => busStationKey = value; }
+
         public BusStation(int code, double _latitude, double _longitude, string _adress = "")
         {
             busStationKey = code;
