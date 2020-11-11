f1d39b90e6acd1d9095ed89a1a063ed7e64a02c4
שרה תמר אמיתי <saratama@g.jct.ac.il>
2020-11-11 22:16:53 +0200

    start of main

--- dotNet5781_02_7195_2621/BusLineStation.cs
+++ dotNet5781_02_7195_2621/BusLineStation.cs
@@ -8,13 +8,14 @@ namespace dotNet5781_02_7195_2621
 {
     class BusLineStation:BusStation
     {
-        static private List<BusStation> allStations = new List<BusStation>();
+        static public List<BusStation> allStations = new List<BusStation>();
         private double distanceFromPrevStat;//distance from previous station
         private TimeSpan timeFromPrevStat;//time from the previous station
         public double DistanceFromPrevStat { get => distanceFromPrevStat; set => distanceFromPrevStat = value; }
         public TimeSpan TimeFromPrevStat { get => timeFromPrevStat; set => timeFromPrevStat = value; }
         public BusLineStation():base()
         {
+            bool found = false;
             foreach (BusStation item in allStations)
             {
                 if (item.BusStationKey == BusStationKey)
@@ -23,14 +24,17 @@ namespace dotNet5781_02_7195_2621
                     {
                         //throw;
                     }
+                    found = true;
                 }
             }
             distanceFromPrevStat = rand.NextDouble() * (150 - 0.1) +0.1;
             timeFromPrevStat =new TimeSpan(0,(int)(distanceFromPrevStat*60/50),0);//?
-            allStations.Add(this);//?
+            if(found==false)
+                allStations.Add(this);//?
         }
         public BusLineStation(int code, double _latitude, double _longitude, string _adress = ""):base( code, _latitude, _longitude,_adress)
         {
+            bool found = false;
             foreach (BusStation item in allStations)
             {
                 if (item.BusStationKey == BusStationKey)
@@ -39,14 +43,17 @@ namespace dotNet5781_02_7195_2621
                     {
                         //throw;
                     }
+                    found = false;
                 }
             }
             distanceFromPrevStat = rand.NextDouble() * (150 - 0.1) + 0.1;
             timeFromPrevStat = new TimeSpan(0, (int)(distanceFromPrevStat * 60 / 50), 0);
-            allStations.Add(this);//?
+            if (found == false)
+                allStations.Add(this);//?
         }
         public BusLineStation(int code, string _adress = ""):base( code, _adress )
         {
+            bool found = false;
             foreach (BusStation item in allStations)
             {
                 if (item.BusStationKey == BusStationKey)
@@ -55,11 +62,13 @@ namespace dotNet5781_02_7195_2621
                     {
                         //throw;
                     }
+                    found = false;
                 }
             }
             distanceFromPrevStat = rand.NextDouble() * (150 - 0.1) + 0.1;
             timeFromPrevStat = new TimeSpan(0, (int)(distanceFromPrevStat * 60 / 50), 0);
-            allStations.Add(this);//?
+            if (found == false)
+                allStations.Add(this);//?
         }
 
     }
