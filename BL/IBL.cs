82f37b54954de54a7161a145b57bc986ba86a099
שרה תמר אמיתי <saratama@g.jct.ac.il>
2020-12-28 18:54:31 +0200

    ibl

--- BL/IBL.cs
+++ BL/IBL.cs
@@ -6,11 +6,32 @@ namespace BLAPI
 {
     public interface IBL
     {
+        #region User
         bool IsAdminAndExists(string userName, string password);
-        IEnumerable<BO.Bus> GetAllBuses();
-        IEnumerable<BO.Line> GetAllLines();
-        IEnumerable<BO.Station> GetAllStations();
+        #endregion
 
+        #region Bus
+        IEnumerable<BO.Bus> GetAllBuses();
+        void DeleteBus(int num);
+        void RefuelBus(int num);
+        void CareBus(int num);
+        void UpdateBus(BO.Bus);
+        void AddBus(int licenseNum, DateTime fromDate, double totalTrip, double fuelRemain, Enums.BusStatus status);
+        #endregion
+
+        #region Line
+        IEnumerable<BO.Line> GetAllLines();
+        void DeleteLine(int num);
+        void UpdateLine(BO.Line);
+        void AddLine(......);
+        #endregion
+
+        #region Station
+        IEnumerable<BO.Station> GetAllStations();
+        void DeleteStation(int num);
+        void UpdateStation(BO.Line);
+        void AddStation(......);
+        #endregion
 
     }
 }
