7aa05ff221dd60cdff1a1337cc79492bd726ee7c
Hana Dachkovsky <dachkovs@g.jct.ac.il>
2020-12-31 12:53:49 +0200

    implimation of add functions dl

--- DalObject/DalObject.cs
+++ DalObject/DalObject.cs
@@ -1,56 +1,118 @@
 ﻿using DLAPI;
 using DO;
+using DS;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DL
 {
     sealed class DLObject : IDAL    //internal
     {
+        #region singelton
+        static readonly DLObject instance = new DLObject();
+        static DLObject() { }// static ctor to ensure instance init is done just before first usage
+        DLObject() { } // default => private
+        public static DLObject Instance { get => instance; }// The public Instance property to use
+        #endregion
+        //לברר בקשר לבונוס של המחיקה!
         public void AddAdjacentStations(AdjacentStations adjacentStations)
         {
-            throw new NotImplementedException();
+            if(DataSource.ListAdjacentStations.FirstOrDefault(adjacent=> adjacent.Station1==adjacentStations.Station1 && adjacent.Station2 == adjacentStations.Station2) !=null)
+            {
+                //throw new 
+            }
+            if(DataSource.ListStation.FirstOrDefault(s=>s.Code==adjacentStations.Station1)==null|| DataSource.ListStation.FirstOrDefault(s => s.Code == adjacentStations.Station2) == null)
+            {
+                //throw new
+            }
+            DataSource.ListAdjacentStations.Add(adjacentStations.Clone());
         }
 
         public void AddBus(Bus bus)
         {
-            throw new NotImplementedException();
+            if (DataSource.ListBus.FirstOrDefault(b => b.LicenseNum == bus.LicenseNum) != null)
+            {
+                //throw new 
+            }
+          
+            DataSource.ListBus.Add(bus.Clone());
         }
 
         public void AddBusOnTrip(BusOnTrip busOnTrip)
         {
-            throw new NotImplementedException();
+            if (DataSource.ListBus.FirstOrDefault(b => b.LicenseNum == busOnTrip.LicenseNum) == null)
+            {
+                //throw new 
+            }
+            if (DataSource.ListLine.FirstOrDefault(b => b.Id == busOnTrip.LineId) == null )
+            {
+                //throw new
+            }
+            //להוסיף חריגה עם תחנה קודמת?
+            busOnTrip.Id = ++Counter.BusOnTripNum;
+            DataSource.ListBusOnTrip.Add(busOnTrip.Clone());
         }
 
         public void AddLine(Line line)
         {
-            throw new NotImplementedException();
+            //חריגות אם תחנות לא קיימות? 
+            line.Id = ++Counter.LineNum;
+            DataSource.ListLine.Add(line.Clone());
         }
 
         public void AddLineStation(LineStation lineStation)
         {
-            throw new NotImplementedException();
+            //חריגה עם תחנה קיימת בקו?
+            if (DataSource.ListStation.FirstOrDefault(s => s.Code == lineStation.Station) == null || DataSource.ListLine.FirstOrDefault(l =>l.Id == lineStation.LineId) == null)
+            {
+                //throw new
+            }
+            DataSource.ListLineStation.Add(lineStation.Clone());
         }
 
         public void AddLineTrip(LineTrip lineTrip)
         {
-            throw new NotImplementedException();
+            if (DataSource.ListLine.FirstOrDefault(l => l.Id == lineTrip.LineId) == null)
+            {
+                //throw new
+            }
+            lineTrip.Id = ++Counter.LineTripNum;
+            DataSource.ListLineTrip.Add(lineTrip.Clone());
         }
 
         public void AddStation(Station station)
         {
-            throw new NotImplementedException();
+            if(DataSource.ListStation.FirstOrDefault(s => s.Code == station.Code) != null)
+            {
+                //throw new
+            }
+            DataSource.ListStation.Add(station.Clone());
         }
 
         public void AddTrip(Trip trip)
         {
-            throw new NotImplementedException();
+            if (DataSource.ListLine.FirstOrDefault(l => l.Id == trip.LineId) == null)
+            {
+                //throw new
+            }
+            if (DataSource.ListUser.FirstOrDefault(u => u.UserName == trip.UserName) == null)
+            {
+                //throw new
+            }
+            //חריגות של תחנה?
+            trip.Id = ++Counter.TripNum;
+            DataSource.ListTrip.Add(trip.Clone());
         }
 
         public void AddUser(User user)
         {
-            throw new NotImplementedException();
+            if (DataSource.ListUser.FirstOrDefault(u => u.UserName == user.UserName) != null)
+            {
+                //throw new
+            }
+            DataSource.ListUser.Add(user.Clone());
         }
 
         public void DeleteAdjacentStations(int station1, int station2)
@@ -60,12 +122,17 @@ namespace DL
 
         public void DeleteBus(int licenseNum)
         {
-            throw new NotImplementedException();
+            DO.Bus bus = DataSource.ListBus.Find(b => b.LicenseNum == licenseNum) ;
+            if(bus==null)
+            {
+                //throw new
+            }
+            DataSource.ListBus.Remove(bus);
         }
 
         public void DeleteBusOnTrip(int id)
         {
-            throw new NotImplementedException();
+            
         }
 
         public void DeleteLine(int id)
